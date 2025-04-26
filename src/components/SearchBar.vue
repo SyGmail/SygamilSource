9c2e39120cf9a4016b4bd1ea014185218c3dd1ff
sy <c3156298376@163.com>
2025-04-26 14:13:19 +0800

    update

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,10 @@
 <!-- 搜索小组件 -->
 <template>
-  <div class="flex rounded-3xl shadow shadow-gray-300 items-center px-2">
+  <div class="flex rounded-3xl shadow shadow-gray-300 items-center px-2 relative">
     <svg class="cursor-pointer" @click="onSearch" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 2h8v2H6zM4 6V4h2v2zm0 8H2V6h2zm2 2H4v-2h2zm8 0v2H6v-2zm2-2h-2v2h2v2h2v2h2v2h2v-2h-2v-2h-2v-2h-2zm0-8h2v8h-2zm0 0V4h-2v2z"/></svg>
-    <input @keydown.enter="onSearch" v-model="keyword" :placeholder="'使用'+nowEngine.title+'嗖嗖...'" type="text" class="select-none outline-none pl-2 flex-1 h-12">
+    <input @keydown.enter="onSearch" v-model="keyword" :placeholder="searchHolder" type="text" class="select-none outline-none pl-2 flex-1 h-12">
 
+    <img :src="nowEngine.icon" class="size-10 absolute select-none right-2 rounded-full">
     <!-- ############################################################### -->
     <!-- Docker栏 -->
     <aside class="opacity-0 scale-80 hover:opacity-100 hover:scale-100 transition select-none fixed bottom-10 left-0 w-screen flex justify-center">
@@ -113,6 +114,8 @@ onMounted(() => {
   });
 })
 
+// input框的提示词
+const searchHolder = ref('想搜点什么?')
 
 // 用户输入的搜索词
 const keyword = ref('');
