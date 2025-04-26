85925395b03798912a2da14510c587e0a5f40160
sy <c3156298376@163.com>
2025-04-26 17:19:01 +0800

    update

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,16 +2,18 @@
 <template>
   <div class="flex rounded-3xl shadow shadow-gray-300 items-center px-2 relative">
     <svg class="cursor-pointer" @click="onSearch" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 2h8v2H6zM4 6V4h2v2zm0 8H2V6h2zm2 2H4v-2h2zm8 0v2H6v-2zm2-2h-2v2h2v2h2v2h2v2h2v-2h-2v-2h-2v-2h-2zm0-8h2v8h-2zm0 0V4h-2v2z"/></svg>
-    <input @keydown.enter="onSearch" v-model="keyword" :placeholder="searchHolder" type="text" class="select-none outline-none pl-2 pr-10 flex-1 h-12">
+    <input ref="inputDoc" @keydown.enter="onSearch" v-model="keyword" :placeholder="searchHolder" type="text" class="select-none outline-none pl-2 pr-10 flex-1 h-12">
 
     <img :src="nowEngine.icon" class="size-10 absolute select-none right-2 rounded-full">
     <!-- ############################################################### -->
     <!-- Docker栏 -->
-    <aside class="opacity-0 scale-80 hover:opacity-100 hover:scale-100 transition select-none fixed bottom-10 left-0 w-screen flex justify-center">
-      <ul id="engineList" class="transition-all duration-500 flex dark:bg-[#3C3939] gap-2 rounded-2xl p-2 items-center justify-center dark:outline outline-2 outline-gray-400/10 dark:outline-gray-100/20 max-h-14">
-          <nav @click="onChangeEngine(engine)" ref="dockerItems" class="z-2 duration-180 flex justify-center items-center w-10 h-10 transition-all" v-for="engine in engineList" :key="engine">
-            <img :src="engine.icon" class="hover:active:brightness-50 transition-all rounded-xl w-10 h-10 duration-180 " :class="engine.style">
-            <span class="invisible font-light absolute -top-13 bg-gray-300 rounded px-2 py-1">{{ engine.title }}</span>
+    <aside class="select-none fixed bottom-10 left-0 w-screen flex justify-center">
+      <ul ref="ulRef" id="engineList" class="opacity-0 hover:opacity-100 transition flex dark:bg-[#3C3939] rounded-2xl p-2 items-center justify-center dark:outline outline-2 outline-gray-400/10 dark:outline-gray-100/20 max-h-13">
+          <nav @click="onChangeEngine(engine)" ref="dockerItems" class="relative z-2 p-1 flex justify-center items-center" v-for="engine in engineList" :key="engine">
+            <img :src="engine.icon" class="hover:active:brightness-50 rounded-xl w-full h-full" :class="engine.style">
+            <div class="invisible font-light absolute -top-13 w-80 flex justify-center">
+              <span class="bg-gray-300 rounded px-2 py-1">{{ engine.title }}</span>
+            </div>
           </nav>
       </ul>
     </aside>
@@ -19,32 +21,23 @@
 </template>
 
 <style scoped>
-/* 暂时为空 */
-nav:hover {
-  width: 80px;
-  height: 80px;
-}
-
-nav:hover img {
-  width: 70px;
-  height: 70px;
-}
-
-.hover_1 {
-  width: 60px;
-  height: 60px;
-}
-.hover_2 {
-  width: 50px;
-  height: 50px;
-}
-
 /* 禁止图片拖拽 */
 img {
   -webkit-user-drag: none;
 }
 
-img:hover + span {
+ul {
+  --scale: 1;
+  --size: 3rem;
+  --move: 3rem;
+}
+
+nav {
+  width: calc(var(--size) * var(--scale));
+  height: calc(var(--size) * var(--scale));
+}
+
+img:hover + div {
   visibility: visible;
 }
 </style>
@@ -55,64 +48,48 @@ import { onMounted, ref } from 'vue';
 // 用户本地数据
 const userStore = useUserStore();
 
+// 搜索框
+const inputDoc = ref(null);
+const ulRef = ref(null)
+
 // 处理悬浮缩放效果
 const dockerItems = ref(null);
 
+// 处理Docker栏动画
 onMounted(() => {
-  dockerItems.value.forEach((item, idx) => {
-    // 添加鼠标进入事件
-    const pre_2 = dockerItems.value[idx-2];
-    const pre_1 = dockerItems.value[idx-1];
-    const nex_1 = dockerItems.value[idx+1];
-    const nex_2 = dockerItems.value[idx+2];
+  dockerItems.value.forEach(nav => {
+    nav.addEventListener('mousemove', (e) => {
+      // item 为鼠标当前悬浮位置的元素
+      const item = e.currentTarget;
+      let itemRect = item.getBoundingClientRect();
+      let offset = Math.abs((e.clientX - itemRect.left) / itemRect.width);
 
-    item.addEventListener('mouseenter', () => {
-      if (pre_2) {
-        const child = pre_2.firstChild;
-        pre_2.classList.add('hover_2')
-        child.classList.add('hover_2')
-      }
-      if (pre_1) {
-        const child = pre_1.firstChild;
-        pre_1.classList.add('hover_1')
-        child.classList.add('hover_1')
-      }
-      if (nex_1) {
-        const child = nex_1.firstChild;
-        nex_1.classList.add('hover_1')
-        child.classList.add('hover_1')
-      }
-      if (nex_2) {
-        const child = nex_2.firstChild;
-        nex_2.classList.add('hover_2')
-        child.classList.add('hover_2')
-      }
-    })
-    // 添加鼠标离开事件
-    item.addEventListener('mouseleave', () => {
-      if (pre_2) {
-        const child = pre_2.firstChild;
-        pre_2.classList.remove('hover_2')
-        child.classList.remove('hover_2')
-      }
-      if (pre_1) {
-        const child = pre_1.firstChild;
-        pre_1.classList.remove('hover_1')
-        child.classList.remove('hover_1')
-      }
-      if (nex_1) {
-        const child = nex_1.firstChild;
-        nex_1.classList.remove('hover_1')
-        child.classList.remove('hover_1')
-      }
-      if (nex_2) {
-        const child = nex_2.firstChild;
-        nex_2.classList.remove('hover_2')
-        child.classList.remove('hover_2')
-      }
-    })
+      const prev = item.previousElementSibling || null;
+      const next = item.nextElementSibling || null;
+      
+      // 最大放大倍率
+      const scale = 0.6;
+      // 为了使在图标最左边的时候缩放效果最大
+      prev?.style.setProperty('--scale', 1 + scale * Math.abs(1 - offset));
+
+      item.style.setProperty('--scale', 1 + scale);
+
+      // 使在图标最右边的时候缩放最大，最左边缩放最小
+      next?.style.setProperty('--scale', 1 + scale * offset);
+    });
   });
+  // 鼠标离开清除放大镜效果
+  ulRef.value.addEventListener('mouseleave', () => {
+    resetScale();
+  })
 })
+
+// 将放大倍率重置
+function resetScale() {
+  dockerItems.value.forEach(nav => {
+    nav.style.setProperty('--scale', 1)
+  })
+}
 
 // input框的提示词
 const searchHolder = ref('想搜点什么?')
@@ -271,6 +248,7 @@ async function onSearch() {
 function onChangeEngine(engine) {
   nowEngine.value = engine;
   updateNowEngine();
+  inputDoc.value.focus()
 }
 
 // 刷新存储的nowEngine
